549b06bf909885e2ae26902bfb01201060ce0e07
chenna333 <59122294+chenna333@users.noreply.github.com>
2025-10-22 17:26:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "uniquenameintra-demo"   # replace later
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"       # optional, for state locking
-    encrypt        = true
-  }
+  backend "s3" {}
 }
-
